9ecba674cb1e7771f2aa976c6272dd0642edad46
gp-abhay <abhay@grampower.com>
2022-12-28 18:09:59 +0530

    song url done

--- src/routers/play_song.py
+++ src/routers/play_song.py
@@ -2,8 +2,15 @@ from fastapi import APIRouter, Depends, status
 from config import get_db
 from sqlalchemy.orm import Session
 from pathlib import Path
-import models, json, utils, schemas
+from os import environ
+import models, json, utils, schemas, boto3
 
+
+S3_CLIENT = boto3.client(
+    's3', aws_access_key_id = environ.get('ACCESS_KEY'),
+    aws_secret_access_key = environ.get('SECRET_KEY'),
+    region_name = environ.get('REGION')
+)
 
 router = APIRouter(prefix='/play-song', tags=['Play song'])
 
@@ -12,9 +19,16 @@ def get_songs(_id: str = '', db: Session = Depends(get_db)):
     '''
         URL (/play-song) is used to Play the song ondemand
     '''
-
     modl = models.SongDetails
-
     songs_data = db.query(modl).filter(modl.id == _id).first()
+
+    obj = S3_CLIENT.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': environ.get('BUCKET'),
+            'Key': f'songs/{"".join(e for e in songs_data.movie_folder if e.isalnum())}/{songs_data.title}.mp3'
+        },
+        ExpiresIn=600
+    )
     
-    return utils.resp_format(songs_data, status.HTTP_200_OK, schemas.GetSongsList)
+    return utils.resp_format(obj, status.HTTP_200_OK)
