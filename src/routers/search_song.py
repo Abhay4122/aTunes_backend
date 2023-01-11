57ca0ad91bcd69acf22dc7bf0fde82477fa98cc0
abhay4122 <abhay4122@gmail.com>
2023-01-11 21:35:00 +0530

    redisearch applied

--- src/routers/search_song.py
+++ src/routers/search_song.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from config import get_db
+from config import get_db, redis_search
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
 import models, json, utils, schemas
@@ -13,12 +13,17 @@ def get_songs(_serch: str = '', db: Session = Depends(get_db)):
     URL (/search-song) is used to seasrch the songs song name, year, artist, gener, movie
   '''
   modl = models.SongDetails
+  r_con = redis_search()
 
   songs_data = []
 
   if _serch:
-    songs_data = db.query(modl).filter(or_(modl.title.ilike(f'%{_serch}%'), modl.movie_name.ilike(f'%{_serch}%'))).all()
+    # songs_data = db.query(modl).filter(or_(modl.title.ilike(f'%{_serch}%'), modl.movie_name.ilike(f'%{_serch}%'))).all()
+    raw_search = r_con.search(f'@title:{_serch}')
+
+    for i in raw_search.docs:
+      songs_data.append(json.loads(i.body))
   else:
     {'msg': 'Sorry, no data found!'}
 
-  return utils.resp_format(songs_data, status.HTTP_200_OK, schemas.GetSongsList)
+  return utils.resp_format(songs_data, status.HTTP_200_OK)
